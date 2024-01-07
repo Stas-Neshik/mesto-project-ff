dd28bad9fd161d1a666716805ef7f5a9dc560f57
StanislavN <s.neshik@yandex.ru>
2024-01-07 18:56:34 +0300

    add Cards and like function

--- scripts/cards.js
+++ scripts/cards.js
@@ -1,26 +1,26 @@
 const initialCards = [
     {
-      name: "Архыз",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
+      name: "Парк штата Огненная Долина, США",
+      link: "https://images.unsplash.com/photo-1500530855697-b586d89ba3ee?q=80&w=3087&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     },
     {
-      name: "Челябинская область",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
+      name: "Иссландия",
+      link: "https://images.unsplash.com/photo-1504893524553-b855bce32c67?q=80&w=3087&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     },
     {
-      name: "Иваново",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg",
+      name: "Долина Йосемити ,США",
+      link: "https://images.unsplash.com/photo-1502512571217-6a08d302fe5a?q=80&w=3088&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    },
+    {
+      name: "Эльбрус",
+      link: "https://images.unsplash.com/photo-1627414363144-8982a29a0922?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    },
+    {
+      name: "Алтай",
+      link: "https://images.unsplash.com/photo-1626538481998-0629afebd684?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     },
     {
       name: "Камчатка",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg",
-    },
-    {
-      name: "Холмогорский район",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg",
-    },
-    {
-      name: "Байкал",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg",
+      link: "https://images.unsplash.com/photo-1535557142533-b5e1cc6e2a5d?q=80&w=3029&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     }
 ];
--- scripts/index.js
+++ scripts/index.js
@@ -2,8 +2,44 @@
 
 // @todo: DOM узлы
 
+const cardList = document.querySelector('.places__list');
+const cardTemplate = document.querySelector('#card-template').content; // Выбрал template, получил содержимое (.content) 
+const card = document.querySelector('.card');
+
+
+
+// const deleteBtn = cardTemplate.querySelector('.card__delete-button');
+// const likeBtn = cardTemplate.querySelector('.card__like-button');
+
 // @todo: Функция создания карточки
+
+initialCards.forEach( function addCard(element) {  // для каждого элемента массива срабатывает колбэк addCard
+  const cardElement = cardTemplate.cloneNode(true); // копирование template карточки
+
+  cardElement.querySelector('.card__image').src = element.link;
+  cardElement.querySelector('.card__image').alt = 'Красивое место';
+  cardElement.querySelector('.card__title').textContent = element.name;
+  cardElement.querySelector('.card__delete-button').addEventListener('click', deleteCard);
+  cardElement.querySelector('.card__like-button').addEventListener('click', function(evt){
+    console.log(evt);
+    // evt.target.classList.toggle('card__like-button_is-active');
+  });
+
+  cardList.append(cardElement);
+
+})
+
+
 
 // @todo: Функция удаления карточки
 
+function deleteCard (item) {
+  item.remove();
+  
+}
+
+
+
+
 // @todo: Вывести карточки на страницу
+
