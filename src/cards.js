1454eeb7f9fd56777b10642ec5589147143fa131
StanislavN <s.neshik@yandex.ru>
2024-01-30 23:23:54 +0300

    html bug

--- src/cards.js
+++ src/cards.js
@@ -24,3 +24,5 @@ const initialCards = [
       link: "https://images.unsplash.com/photo-1535557142533-b5e1cc6e2a5d?q=80&w=3029&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     }
 ];
+
+module.exports = { initialCards }; 
--- src/index.js
+++ src/index.js
@@ -1,44 +1,32 @@
-'use strict';
-// @todo: Темплейт карточки
-
-// @todo: DOM узлы
-
+import '../pages/index.css';
 const cardList = document.querySelector('.places__list');
-  // const cardTemplate = document.getElementById('card-template').content;
-  // const cardElement = cardTemplate.cloneNode(true); 
 
-// @todo: Функция создания карточки
-
+// Функция создания карточки
 
 const createCard = (cardData, deleteCallback) => {
 
   const cardTemplate = document.getElementById('card-template').content;
   const cardElement = cardTemplate.cloneNode(true);
-  const deleteBtn = cardElement.querySelector('.card__delete-button');
 
   cardElement.querySelector('.card__image').src = cardData.link;
   cardElement.querySelector('.card__image').alt = cardData.name;
   cardElement.querySelector('.card__title').textContent = cardData.name;
+  cardElement.querySelector('.card__delete-button').addEventListener('click', deleteCallback);
   cardElement.querySelector('.card__like-button').addEventListener('click', function(evt){
     evt.target.classList.toggle('card__like-button_is-active');
   });
-  deleteBtn.addEventListener('click', deleteCallback)
   
   return cardElement;
 }
 
-// @todo: Функция удаления карточки
+// Функция удаления карточки
 
-function deleteCard (evt){ 
-  console.log('hello');
+function deleteCard (evt) { 
   const listItem = evt.target.closest('.card');
-
   listItem.remove();
-  // const listItem = deleteBtn.closest('.card')
-  // listItem.remove();
 } 
 
-// @todo: Вывести карточки на страницу
+// Вывести карточки на страницу
 
 initialCards.forEach(function addCard(element){
   cardList.append(createCard(element, deleteCard));
