08fe083d8f6d017a950872f9f49288cea3c2ef08
StanislavN <s.neshik@yandex.ru>
2024-02-24 22:36:44 +0300

    End part 1 of validate, btn + custom Message

--- src/validation.js
+++ src/validation.js
@@ -1,13 +1,13 @@
+
+// Показ ошибки
 function showInputError (form, input, errorMessage) {
   const errorElement = form.querySelector(`.${input.id}-error`);
   errorElement.classList.add('form__input-error_active');
   input.classList.add('popup__input_error');
-
   errorElement.textContent = errorMessage;
-
-
 };
 
+// Удаление ошибки
 function hideInputError (form, input) {
   const errorElement = form.querySelector(`.${input.id}-error`);
   input.classList.remove('popup__input_error');
@@ -15,39 +15,41 @@ function hideInputError (form, input) {
   errorElement.textContent = '';
 };
 
+// Проверка валидации, кастомная ошибка + обычная
+
 function checkInputValidity (form, input) {
+
+  if (input.validity.patternMismatch) {
+    input.setCustomValidity(input.dataset.errorMessage);
+} 
+  else input.setCustomValidity("");
+
   if (!input.validity.valid) {
-     showInputError(form, input, input.validationMessage)
+     showInputError(form, input, input.validationMessage);
   }
-  else hideInputError(form, input)
+  else hideInputError(form, input);
 };
 
 
-
+  // Проверка валидации для кнопки (возвращает true (?) в зависимости от того как прошла валидация)
 function hasInvalidInput (inputList) {
   return inputList.some(inputElement => {
     return !inputElement.validity.valid;
   });
 };
 
-
+  // Закрашиваем кнопку в зависимости от проверки валидации формы
 function toggleButtonState (inputList, buttonElement) {
+
   if (hasInvalidInput(inputList)) {
     buttonElement.disabled = true;
-    console.log('toggleButtonState работает')
-    // buttonElement.classList.add('');
+     buttonElement.classList.add('popup__button_inactive');
   }
   else {
     buttonElement.disabled = false;
-    console.log('toggleButtonState работает')
-    // buttonElement.classList.remove('');
+    buttonElement.classList.remove('popup__button_inactive');
   };
 };
-
-
-// На чем закончил = сделал функцию про кнопку! посмотри еще раз ее. на сейчас все работает.
-// Нужно сделать класс для неактивной кнопки, потом начать делать кастомные предложения по валидации.
-
 
 
 
@@ -74,4 +76,4 @@ function enableValidation () {
 }
 
 
- export {checkInputValidity, enableValidation};
+ export {enableValidation};
